353a97b4756288fce2b2f97050025e8ed9e2e67f
Chris Guidry <chris@theguidrys.us>
2022-06-21 22:03:43 -0400

    Angles as dimensionless Units, but still distinct. (#12)

--- tests/si/test_angles.py
+++ tests/si/test_angles.py
@@ -1,20 +1,46 @@
-from measured import One
-from measured.si import Meter, Radian, Steradian
+from math import pi as π
+
+import pytest
+
+from measured import Number, One, PlaneAngle, Quantity, SolidAngle
+from measured.si import Arcminute, Arcsecond, Degree, Radian, Steradian
 
 
-def test_radian_is_dimensionless() -> None:
-    assert Radian == Meter / Meter
-    assert Radian == One
-    assert Radian.name == "one"
-    assert Radian.symbol == "1"
-    assert Radian.names == ["one", "radian", "steradian"]
-    assert Radian.symbols == ["1", "rad", "sr"]
+def test_radian_is_dimensionless_but_unique() -> None:
+    assert Radian.dimension is Number
+    assert Radian.dimension is PlaneAngle
+    assert Radian != One
+    assert Radian != Steradian
+    assert Radian.name == "radian"
+    assert Radian.symbol == "rad"
 
 
-def test_steradian_is_dimensionless() -> None:
-    assert Steradian == Meter / Meter
-    assert Steradian == One
-    assert Steradian.name == "one"
-    assert Steradian.symbol == "1"
-    assert Steradian.names == ["one", "radian", "steradian"]
-    assert Steradian.symbols == ["1", "rad", "sr"]
+def test_steradian_is_dimensionless_but_unique() -> None:
+    assert Steradian.dimension is Number
+    assert Steradian.dimension is SolidAngle
+    assert Steradian != One
+    assert Steradian != Radian
+    assert Steradian.name == "steradian"
+    assert Steradian.symbol == "sr"
+
+
+@pytest.mark.parametrize(
+    "left, right",
+    [
+        (0 * Radian, 0 * Degree),
+        (π / 4 * Radian, 45 * Degree),
+        (1 * Radian, 180 / π * Degree),
+        (π / 2 * Radian, 90 * Degree),
+        (2 * Radian, 360 / π * Degree),
+        (π * Radian, 180 * Degree),
+        (3 * π / 2 * Radian, 270 * Degree),
+        (2 * π * Radian, 360 * Degree),
+        (1 * Arcminute, 60 * Arcsecond),
+        (1 * Degree, 60 * Arcminute),
+        (1 * Arcsecond, 0.000004848136 * Radian),
+        (1 * Arcminute, 0.000290888 * Radian),
+    ],
+)
+def test_angle_conversions(left: Quantity, right: Quantity) -> None:
+    left.assert_approximates(right)
+    right.assert_approximates(left)
