16b819e7e88a88b7bf61e06ec85bdb3e992abea2
Chris Guidry <chris@theguidrys.us>
2022-06-25 20:51:41 -0400

    20x improvement from caching conversions.

--- tests/performance_harness.py
+++ tests/performance_harness.py
@@ -6,8 +6,9 @@ from typing import Callable
 
 import typer
 
-from measured import Quantity
+from measured import One, Quantity
 from measured.si import Ampere, Meter, Ohm, Volt
+from measured.us import Ounce, Ton
 
 app = typer.Typer()
 options = {"baseline": False}
@@ -63,6 +64,18 @@ def resistances() -> None:
     for a in (Quantity(a, Ampere) for a in range(1, 1001)):
         for v in (Quantity(v, Volt) for v in range(1, 1001)):
             assert v / a == (v.magnitude / a.magnitude) * Ohm
+
+
+@app.command()
+@profiled
+def conversions() -> None:
+    for o in (Quantity(o, Ounce) for o in range(1, 1001)):
+        for t in (Quantity(t, Ton) for t in range(1, 1001)):
+            divided = (t / o).in_unit(One)
+            assert divided.unit == One
+            assert round(divided.magnitude) == round(
+                t.magnitude / o.magnitude * 20 * 100 * 16
+            )
 
 
 if __name__ == "__main__":
