9f1113497d54450dc8c7baa73fbc25e3f351d815
Chris Guidry <chris@theguidrys.us>
2022-07-30 07:59:46 -0400

    Adding an additional test for sound power versus pressure

--- tests/test_acoustics.py
+++ tests/test_acoustics.py
@@ -1,8 +1,10 @@
 import pytest
+from pytest import approx
 
-from measured import Level, Pressure, Quantity, approximately
-from measured.acoustics import ABSOLUTE_THRESHOLD_OF_HEARING, dBSPL
-from measured.si import Micro, Pascal
+from measured import Decibel, Level, Pressure, Quantity, approximately
+from measured.acoustics import ABSOLUTE_THRESHOLD_OF_HEARING, dBSPL, dBSWL
+from measured.geometry import π
+from measured.si import Meter, Micro, Pascal
 
 
 def test_auditory_threshold() -> None:
@@ -32,3 +34,23 @@ def test_examples_of_sound_pressure(
 ) -> None:
     assert pressure == approximately(level, within=within)
     assert level == approximately(pressure, within=within)
+
+
+def test_sound_power_from_sound_pressure() -> None:
+    # https://en.wikipedia.org/wiki/Sound_power#Relationship_with_sound_pressure_level
+    # https://en.wikipedia.org/wiki/Sound_pressure#Sound_pressure_level
+
+    # cross-checked with
+    # https://www.wkcgroup.com/tools-room/sound-power-sound-pressure-converter/
+
+    distance = 2 * Meter
+    area = 4 * π * distance**2
+    assert area == approximately(50.265482 * Meter**2)
+
+    surface_area_level = area.level(Decibel[1 * Meter**2])
+    assert surface_area_level == approximately(17.01269855 * Decibel[1 * Meter**2])
+
+    pressure = 50 * dBSPL
+    power = 67.0127 * dBSWL
+
+    assert pressure.magnitude + surface_area_level.magnitude == approx(power.magnitude)
